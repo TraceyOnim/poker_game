4f323d56ecc7d89e20d8fc055ab985986fd73281
TraceyOnim <traceyonim22@gmail.com>
2021-10-28 17:02:05 +0300

    display highest player rank

--- lib/poker.ex
+++ lib/poker.ex
@@ -14,6 +14,10 @@ defmodule Poker do
     :straight_flush
   ]
 
+  def higher_rank_player(%{first_player: first_player, second_player: second_player}) do
+    _higher_rank_player(player_rank(first_player), player_rank(second_player))
+  end
+
   def new(hand, player) do
     %__MODULE__{
       suits: suits(hand),
@@ -24,88 +28,193 @@ defmodule Poker do
     }
   end
 
-  # def higher_rank(%{first_player: first_player, second_player: second_player}) do
-  #   _higher_rank(player_rank(first_player), player_rank(second_player))
-  # end
+  def player_rank({hand, player}) do
+    hand
+    |> new(player)
+    |> rank()
+    |> rank_points()
+  end
 
-  # defp _higher_rank(
-  #        %Poker{rank: {first_rank, first_rank_point}} = first_player,
-  #        %Poker{rank: {second_rank, second_rank_point}} = second_player
-  #      ) do
-  #   if first_rank_point == second_rank_point do
-  #     "===================="
-  #     _high_value_from_similar_rank(first_player, second_player)
-  #   else
-  #     max_point = max(first_rank_point, second_rank_point)
+  def rank(%{suits: [suit | _] = suits, values: values} = poker_deck) do
+    same_suits? = Enum.all?(suits, fn s -> String.equivalent?(s, suit) end)
 
-  #     Enum.find([first_player.rank, second_player.rank], fn {_rank, point} ->
-  #       point == max_point
-  #     end)
-  #   end
-  # end
+    rank =
+      if same_suits? do
+        flush_or_straight_flush(values)
+      else
+        rank(values)
+      end
 
-  # defp _high_value_from_similar_rank(
-  #        %Poker{rank: {first_rank, first_rank_point}} = first_player,
-  #        %Poker{rank: {second_rank, second_rank_point}} = second_player
-  #      ) do
-  #   case first_rank && second_rank do
-  #     :high_card ->
-  #       [
-  #         max_player_value(first_player.values, first_player.player),
-  #         max_player_value(second_player.values, second_player.player)
-  #       ]
-  #       |> Enum.max_by(fn {_value, index, _player} -> index end)
-  #       |> Tuple.insert_at(0, :high_card)
+    %{poker_deck | rank: rank}
+  end
 
-  #     :pair ->
-  #       if pair_index(first_player.values) == pair_index(second_player.values) do
-  #         [
-  #           max_player_value(Enum.uniq(first_player.values), first_player.player),
-  #           max_player_value(Enum.uniq(second_player.values), second_player.player)
-  #         ]
-  #         |> Enum.max_by(fn {_value, index, _player} -> index end)
-  #         |> Tuple.insert_at(0, :high_card)
-  #       end
-  #   end
-  # end
+  def rank(values, rank \\ :none) do
+    {_values, rank} =
+      {values, rank}
+      |> rank_as_full_house()
+      |> rank_as_pair()
+      |> rank_as_two_pair()
+      |> rank_as_three_of_a_kind()
+      |> rank_as_straight()
+      |> rank_as_four_of_a_kind()
 
-  def highest_value(player1_values, player2_values, :high_card_or_flush_or_straight) do
+    case rank do
+      :none -> :high_card
+      rank -> rank
+    end
+  end
+
+  defp rank_points(%{rank: rank} = poker_deck) do
+    rank =
+      @rank
+      |> Enum.zip(1..9)
+      |> Enum.find(fn {rank_name, _point} -> rank_name == rank end)
+
+    %{poker_deck | rank: rank}
+  end
+
+  def _higher_rank_player(
+        %Poker{rank: {first_rank, first_rank_point}} = first_player,
+        %Poker{rank: {second_rank, second_rank_point}} = second_player
+      ) do
+    if first_rank_point == second_rank_point do
+      game_result(first_player, second_player)
+    else
+      max_point = max(first_rank_point, second_rank_point)
+      player1_rank = first_player.rank |> Tuple.append(first_player.player)
+      player2_rank = second_player.rank |> Tuple.append(second_player.player)
+
+      {rank, value, player} =
+        Enum.find([player1_rank, player2_rank], fn {_rank, point, _player} ->
+          point == max_point
+        end)
+
+      "#{player} wins - #{rank}: #{value}"
+    end
+  end
+
+  def game_result(
+        %Poker{rank: {first_rank, first_rank_point}} = first,
+        %Poker{rank: {second_rank, second_rank_point}} = second
+      ) do
+    case first_rank && second_rank do
+      :high_card ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :high_card_or_flush_or_straight)
+        |> game_result(:high_card)
+
+      :straight_flush ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :high_card_or_flush_or_straight)
+        |> game_result(:straight_flush)
+
+      :flush ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :high_card_or_flush_or_straight)
+        |> game_result(:flush)
+
+      :straight ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :high_card_or_flush_or_straight)
+        |> game_result(:straight)
+
+      :three_of_a_kind ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :three_of_a_kind_or_full_house)
+        |> game_result(:three_of_a_kind)
+
+      :full_house ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :three_of_a_kind_or_full_house)
+        |> game_result(:full_house)
+
+      :four_of_a_kind ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :four_of_a_kind)
+        |> game_result(:four_of_a_kind)
+
+      :two_pair ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :two_pair)
+        |> game_result(:two_pair)
+
+      :pair ->
+        {first.values, first.player}
+        |> highest_value({second.values, second.player}, :pair)
+        |> game_result(:pair)
+    end
+  end
+
+  def game_result({_index, [value], player}, rank) do
+    "#{player} wins - #{rank}: #{elem(value, 0)}"
+  end
+
+  def highest_value(
+        {player1_values, player1},
+        {player2_values, player2},
+        :high_card_or_flush_or_straight
+      ) do
+    player1_rank = player1_values |> highest_value(:high_card) |> Tuple.append(player1)
+    player2_rank = player2_values |> highest_value(:high_card) |> Tuple.append(player2)
+
     Enum.max_by(
-      [highest_value(player1_values, :high_card), highest_value(player2_values, :high_card)],
-      fn {index, value} -> index end
+      [player1_rank, player2_rank],
+      fn {index, value, player} -> index end
     )
   end
 
-  def highest_value(player1_values, player2_values, :three_of_a_kind_or_full_house) do
+  def highest_value(
+        {player1_values, player1},
+        {player2_values, player2},
+        :three_of_a_kind_or_full_house
+      ) do
+    [player1_rank] = tripple_value(player1_values)
+    [player2_rank] = tripple_value(player2_values)
+
+    player1_rank = player1_rank |> Tuple.append(player1)
+    player2_rank = player2_rank |> Tuple.append(player2)
+
     Enum.max_by(
-      tripple_value(player1_values) ++ tripple_value(player2_values),
-      fn {index, _value} -> index end
+      [player1_rank, player2_rank],
+      fn {index, value, player} -> index end
     )
   end
 
-  def highest_value(player1_values, player2_values, :four_of_a_kind) do
+  def highest_value({player1_values, player1}, {player2_values, player2}, :four_of_a_kind) do
+    [player1_rank] = quad_value(player1_values)
+    [player2_rank] = quad_value(player2_values)
+
+    player1_rank = player1_rank |> Tuple.append(player1)
+    player2_rank = player2_rank |> Tuple.append(player2)
+
     Enum.max_by(
-      quad_value(player1_values) ++ quad_value(player2_values),
-      fn {index, _value} -> index end
+      [player1_rank, player2_rank],
+      fn {index, _value, _player} -> index end
     )
   end
 
-  def highest_value(player1_values, player2_values, :pair) do
+  def highest_value({player1_values, player1}, {player2_values, player2}, :pair) do
     [{player1_index, _pair_values}] = pair_value(player1_values)
     [{player2_index, _pair_values}] = pair_value(player2_values)
 
     if player1_index == player2_index do
-      player1_highest_card = highest_value(player1_values, :pair)
-      player2_highest_card = highest_value(player2_values, :pair)
+      player1_rank = player1_values |> highest_value(:pair) |> Tuple.append(player1)
+      player2_rank = player2_values |> highest_value(:pair) |> Tuple.append(player2)
 
       Enum.max_by(
-        [highest_value(player1_values, :pair), highest_value(player2_values, :pair)],
-        fn {index, _value} -> index end
+        [player1_rank, player2_rank],
+        fn {index, _value, _player} -> index end
       )
     else
+      [player1_rank] = pair_value(player1_values)
+      [player2_rank] = pair_value(player2_values)
+
+      player1_rank = player1_rank |> Tuple.append(player1)
+      player2_rank = player2_rank |> Tuple.append(player2)
+
       Enum.max_by(
-        pair_value(player1_values) ++ pair_value(player2_values),
-        fn {index, _value} -> index end
+        [player1_rank, player2_rank],
+        fn {index, _value, _player} -> index end
       )
     end
   end
@@ -114,7 +223,7 @@ defmodule Poker do
     highest_two_pair(player1_values, player2_values, :max_two_pair)
   end
 
-  def highest_two_pair(player1_values, player2_values, :max_two_pair) do
+  def highest_two_pair({player1_values, player1}, {player2_values, player2}, :max_two_pair) do
     # max pair value
     {player1_index, _pair_values} = max_pair_value(player1_values, :two_pair)
     {player2_index, _pair_values} = max_pair_value(player2_values, :two_pair)
@@ -122,27 +231,36 @@ defmodule Poker do
     if player1_index == player2_index do
       highest_two_pair(player1_values, player2_values, :min_two_pair)
     else
+      player1_rank = player1_values |> max_pair_value(:two_pair) |> Tuple.append(player1)
+      player2_rank = player2_values |> max_pair_value(:two_pair) |> Tuple.append(player2)
+
       Enum.max_by(
-        [max_pair_value(player1_values, :two_pair), max_pair_value(player2_values, :two_pair)],
-        fn {index, _value} -> index end
+        [player1_rank, player2_rank],
+        fn {index, _value, _player} -> index end
       )
     end
   end
 
-  def highest_two_pair(player1_values, player2_values, :min_two_pair) do
+  def highest_two_pair({player1_values, player1}, {player2_values, player2}, :min_two_pair) do
     # min pair value
     {player1_index, _pair_values} = min_pair_value(player1_values, :two_pair)
     {player2_index, _pair_values} = min_pair_value(player2_values, :two_pair)
 
     if player1_index == player2_index do
+      player1_rank = player1_values |> highest_value(:pair) |> Tuple.append(player1)
+      player2_rank = player2_values |> highest_value(:pair) |> Tuple.append(player2)
+
       Enum.max_by(
-        [highest_value(player1_values, :pair), highest_value(player2_values, :pair)],
-        fn {index, _value} -> index end
+        [player1_rank, player2_rank],
+        fn {index, _value, _player} -> index end
       )
     else
+      player1_rank = player1_values |> min_pair_value(:two_pair) |> Tuple.append(player1)
+      player2_rank = player2_values |> min_pair_value(:two_pair) |> Tuple.append(player2)
+
       Enum.max_by(
-        [min_pair_value(player1_values, :two_pair), min_pair_value(player2_values, :two_pair)],
-        fn {index, _value} -> index end
+        [player1_rank, player2_rank],
+        fn {index, _value, _player} -> index end
       )
     end
   end
@@ -198,42 +316,6 @@ defmodule Poker do
     end
     |> List.flatten()
     |> Enum.group_by(fn {_k, index} -> index end)
-  end
-
-  def player_rank({hand, player}) do
-    hand
-    |> new(player)
-    |> rank()
-    |> rank_points()
-  end
-
-  def rank(%{suits: [suit | _] = suits, values: values} = poker_deck) do
-    same_suits? = Enum.all?(suits, fn s -> String.equivalent?(s, suit) end)
-
-    rank =
-      if same_suits? do
-        flush_or_straight_flush(values)
-      else
-        rank(values)
-      end
-
-    %{poker_deck | rank: rank}
-  end
-
-  def rank(values, rank \\ :none) do
-    {_values, rank} =
-      {values, rank}
-      |> rank_as_full_house()
-      |> rank_as_pair()
-      |> rank_as_two_pair()
-      |> rank_as_three_of_a_kind()
-      |> rank_as_straight()
-      |> rank_as_four_of_a_kind()
-
-    case rank do
-      :none -> :high_card
-      rank -> rank
-    end
   end
 
   defp rank_as_full_house({values, :none = rank}) do
@@ -342,15 +424,6 @@ defmodule Poker do
     end)
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.all?(fn [x, y] -> y == x + 1 end)
-  end
-
-  defp rank_points(%{rank: rank} = poker_deck) do
-    rank =
-      @rank
-      |> Enum.zip(1..9)
-      |> Enum.find(fn {rank_name, _point} -> rank_name == rank end)
-
-    %{poker_deck | rank: rank}
   end
 
   defp suits(hand) do
